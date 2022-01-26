e42581b008f5d3569724ce0c533c64ec7bf0dcdd
evertonsavio <everluca@outlook.com>
2022-01-26 20:33:50 -0300

    :sparkles: read key values

---
+++ redisson/src/test/java/dev/evertonsavio/app/redisson/test/Lec04BucketsAsMapTest.java
@@ -0,0 +1,21 @@
+package dev.evertonsavio.app.redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.client.codec.StringCodec;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec04BucketsAsMapTest extends BaseTest{
+
+    @Test
+    public void bucketAsMap(){
+        Mono<Void> then = this.client.getBuckets(StringCodec.INSTANCE)
+                .get("user:1:name", "user:2:name", "user:3:name")
+                .doOnNext(System.out::println)
+                .then();
+
+        StepVerifier.create(then)
+                .verifyComplete();
+    }
+
+}
