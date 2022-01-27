d57b619b165b76da81986e59bccdfe42ac4fa41b
evertonsavio <everluca@outlook.com>
2022-01-27 13:35:10 -0300

    :sparkles: deleted event listener

--- redisson/src/test/java/dev/evertonsavio/app/redisson/test/Lec05KeyListenerTest.java
+++ redisson/src/test/java/dev/evertonsavio/app/redisson/test/Lec05KeyListenerTest.java
@@ -1,6 +1,7 @@
 package dev.evertonsavio.app.redisson.test;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.DeletedObjectListener;
 import org.redisson.api.ExpiredObjectListener;
 import org.redisson.api.RBucketReactive;
 import org.redisson.client.codec.StringCodec;
@@ -10,23 +11,39 @@ import reactor.test.StepVerifier;
 import java.util.concurrent.TimeUnit;
 
 public class Lec05KeyListenerTest extends BaseTest{
+
+    //NEED THIS CONFIG ON REDIS TO ADD LISTENER
     //config set notify-keyspace-events AKE
     @Test
     public void expiredEventTest(){
         RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
-        Mono<Void> set = bucket.set("sam", 10, TimeUnit.SECONDS);
+        Mono<Void> set = bucket.set("sam", 5, TimeUnit.SECONDS);
         Mono<Void> get = bucket.get()
                 .doOnNext(System.out::println)
                 .then();
 
-        Mono<Void> then = bucket.addListener((ExpiredObjectListener) s -> {
-            System.out.println("Expired " + s);
-        }).then();
+        Mono<Void> then = bucket.addListener((ExpiredObjectListener) s -> System.out.println("Expired " + s)).then();
 
         StepVerifier.create(set.concatWith(get).concatWith(then))
                 .verifyComplete();
 
-        sleep(11000);
+        sleep(6000);
+    }
+
+    @Test
+    public void deletedEventTest(){
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam");
+        Mono<Void> get = bucket.get()
+                .doOnNext(System.out::println)
+                .then();
+
+        Mono<Void> then = bucket.addListener((DeletedObjectListener) s -> System.out.println("Deleted " + s)).then();
+
+        StepVerifier.create(set.concatWith(get).concatWith(then))
+                .verifyComplete();
+
+        sleep(10000);
     }
 
 }
