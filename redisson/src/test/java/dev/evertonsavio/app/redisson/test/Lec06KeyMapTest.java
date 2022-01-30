5797ecb4a5bb43827b41e77f5942be8c5b6bdd70
evertonsavio <everluca@outlook.com>
2022-01-30 08:55:51 -0300

    :sparkles: map of a class

--- redisson/src/test/java/dev/evertonsavio/app/redisson/test/Lec06KeyMapTest.java
+++ redisson/src/test/java/dev/evertonsavio/app/redisson/test/Lec06KeyMapTest.java
@@ -1,11 +1,15 @@
 package dev.evertonsavio.app.redisson.test;
 
+import dev.evertonsavio.app.redisson.dto.Student;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RMapReactive;
 import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class Lec06KeyMapTest extends BaseTest{
@@ -39,7 +43,22 @@ public class Lec06KeyMapTest extends BaseTest{
 
         StepVerifier.create(map.putAll(javaMap).then())
                 .verifyComplete();
+    }
 
+    @Test
+    public void mapTest3(){
+        // Map<Integer,Student>
+        TypedJsonJacksonCodec codec = new TypedJsonJacksonCodec(Integer.class, Student.class);
+        RMapReactive<Integer, Student> map = this.client.getMap("users", codec);
+
+        Student student1 = new Student("sam", 10, "atlanta", List.of(1,2,3));
+        Student student2 = new Student("jake", 12, "miami", List.of(2,1,3));
+
+        Mono<Student> mono1 = map.put(1, student1);
+        Mono<Student> mono2 = map.put(2, student2);
+
+        StepVerifier.create(mono1.concatWith(mono2).then())
+                .verifyComplete();
     }
 
 }
